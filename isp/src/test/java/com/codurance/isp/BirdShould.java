7cbe9d7771d33e00421bba5c347f26ccb2c53904
Kabirohi <155749037+Kabirohi@users.noreply.github.com>
2026-05-14 23:11:50 +0600

    Update BirdShould.java

--- isp/src/test/java/com/codurance/isp/BirdShould.java
+++ isp/src/test/java/com/codurance/isp/BirdShould.java
@@ -17,17 +17,22 @@ class BirdShould {
         System.setOut(new PrintStream(consoleContent));
     }
 
-
     @Test
-    void run() {
-        bird.run();
+    void run_as_a_running_capable_animal() {
+        // Enforces that Bird strictly implements the Running interface
+        Running runningBird = bird; 
+        runningBird.run();
+        
         assertThat(consoleContent.toString())
                 .isEqualTo("Bird is running");
     }
 
     @Test
-    void fly() {
-        bird.fly();
+    void fly_as_a_flying_capable_animal() {
+        // Enforces that Bird strictly implements the Flying interface
+        Flying flyingBird = bird; 
+        flyingBird.fly();
+        
         assertThat(consoleContent.toString())
                 .isEqualTo("Bird is flying");
     }
