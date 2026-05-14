e6ede94da8442659a731487c926fd8d7df3a476e
Kabirohi <155749037+Kabirohi@users.noreply.github.com>
2026-05-14 23:26:20 +0600

    Update ElectricCar.java

--- lsp/src/main/java/com/codurance/lsp/ElectricCar.java
+++ lsp/src/main/java/com/codurance/lsp/ElectricCar.java
@@ -1,18 +1,12 @@
 package com.codurance.lsp;
 
-public class ElectricCar extends Vehicle{
-
+public class ElectricCar extends Vehicle implements ElectricVehicle {
     private static final int BATTERY_FULL = 100;
     private int batteryLevel;
 
     @Override
-    public void fillUpWithFuel() {
-        throw new UnsupportedOperationException("It's an electric car");
-    }
-
-    @Override
     public void chargeBattery() {
-        batteryLevel = BATTERY_FULL;
+        this.batteryLevel = BATTERY_FULL;
     }
 
     public int batteryLevel() {
